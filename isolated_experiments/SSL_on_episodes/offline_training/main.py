c28166e7a405925c72bfbd57f0eee81abb7aa62e
jhairgallardo <jhairgallardo@gmail.com>
2024-06-10 18:17:02 -0400

    fix commented aug

--- isolated_experiments/SSL_on_episodes/offline_training/main.py
+++ isolated_experiments/SSL_on_episodes/offline_training/main.py
@@ -353,7 +353,7 @@ class Transformations:
                 ])
         self.aug = transforms.Compose([
             transforms.RandomResizedCrop(224),# interpolation=Image.BICUBIC),
-            # transforms.RandomHorizontalFlip(p=0.5), ###########################################
+            transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomApply(
                 [transforms.ColorJitter(brightness=0.4, contrast=0.4,
                                         saturation=0.2, hue=0.1)],
