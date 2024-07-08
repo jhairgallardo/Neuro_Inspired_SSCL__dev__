20187b9c623936a16d4fb2259cea7a430e567e29
jhairgallardo <jhairgallardo@gmail.com>
2024-07-07 22:51:38 -0400

    Add crops sampling

--- isolated_experiments/Training_ZCA_PCA_layer/imagenet100/supervised_training/saliency_map.py
+++ isolated_experiments/Training_ZCA_PCA_layer/imagenet100/supervised_training/saliency_map.py
@@ -16,7 +16,7 @@ from scipy.stats import norm
 import cv2
 from multiprocessing import Pool, cpu_count
 
-import json, os
+import json, os, math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -34,6 +34,32 @@ def fit_gennorm_to_batch(feats):
         ggd_params = pool.map(fit_gennorm, [flattened_feats[:, j] for j in range(num_filters)])
     
     return ggd_params
+
+# def compute_saliency_map_ggd_batch(features, ggd_params, weighted=False):
+#     features = features.to(torch.float64)
+#     batch_size, num_filters, height, width = features.shape
+
+#     # Compute the joint probability of all filters for each image in the batch
+#     saliency_maps = torch.ones((batch_size, height, width), dtype=torch.float64)
+#     for i in range(num_filters):
+#         theta, loc, sigma = ggd_params[i]
+#         if weighted:
+#             theta_inv = 1.0 / theta
+#             gamma_incomplete = gammainc(theta_inv, (torch.abs(features[:, i, :, :]) ** theta) * (sigma ** -theta))
+#             gamma_func = gamma(theta_inv)
+#             improved_feats = gamma_incomplete / gamma_func
+#             p = gennorm.pdf(improved_feats.flatten(), theta, loc, sigma)
+#         else:
+#             p = gennorm.pdf(features[:, i, :, :].flatten(), theta, loc, sigma)
+#         p = p.reshape(batch_size, height, width)
+#         saliency_maps *= torch.tensor(p, dtype=torch.float64)
+#     saliency_maps = 1 / (saliency_maps + 1e-5)
+#     saliency_maps /= saliency_maps.sum(dim=(1, 2), keepdim=True)
+#     saliency_maps = saliency_maps.to(torch.float32)
+#     return saliency_maps
+
+
+
 
 def compute_saliency_map_ggd_batch(features, ggd_params, weighted=False):
     features = features.to(torch.float64)
@@ -67,7 +93,9 @@ def compute_saliency_map_ggd_batch(features, ggd_params, weighted=False):
         saliency_map /= np.sum(saliency_map)
         saliency_maps[b] = torch.tensor(saliency_map)
     saliency_maps = saliency_maps.to(torch.float32)
-    return torch.tensor(saliency_maps)
+    return saliency_maps
+
+
 
 def mean_pooling_batch(feats, kernel_size, stride=1, padding=0):
     # feats: tensor of shape (batch_size, num_filters, height, width)
@@ -92,8 +120,79 @@ def resize_saliency_map_batch(saliency_maps, original_shape=(224,224)):
     resized_saliency_maps /= batch_sums.view(-1, 1, 1)  # Normalize using broadcasting
     return resized_saliency_maps
 
+def smart_crop_batch(feature_maps, scale = [0.08, 1.0], ratio = [3.0/4.0, 4.0/3.0], crop_size=None):
+    batch_size, height, width = feature_maps.shape
+    area = height * width
+    log_ratio = torch.log(torch.tensor(ratio))
+
+    all_crops = torch.zeros(batch_size, 4, dtype=torch.int32)
+    
+    for b in range(batch_size):
+        saliency_map = feature_maps[b].cpu().numpy()
+
+        # Get w_crop and h_crop (size of crop)
+        for i in range(10):
+            if crop_size is None:
+                target_area = area * torch.empty(1).uniform_(scale[0], scale[1]).item()
+                aspect_ratio = torch.exp(torch.empty(1).uniform_(log_ratio[0], log_ratio[1])).item()
+                h_crop = int(round(math.sqrt(target_area / aspect_ratio)))
+                w_crop = int(round(math.sqrt(target_area * aspect_ratio)))
+            else:
+                h_crop, w_crop = crop_size
+            
+            if 0 < h_crop <= height and 0 < w_crop <= width:
+                break
+            elif i == 9: # if it fails 10 times, then just take the whole image
+                h_crop = height
+                w_crop = width
+
+        if  h_crop == height and w_crop == width: # if the whole image is the crop, save time by not sampling position
+            all_crops[b] = torch.tensor([0, 0, h_crop, w_crop])
+            continue
+
+        # Get idx_x and idx_y (top left corner of crop). Use saliency map as probability distribution
+        for i in range(10):
+            idx = np.random.choice(np.arange(len(saliency_map.flatten())), p=saliency_map.flatten())
+            idx_cy, idx_cx = np.unravel_index(idx, saliency_map.shape) # center of crop
+            
+            # idx_cy, idx_cx = np.unravel_index(np.argmax(saliency_map.flatten()), saliency_map.shape) # center of crop (get position with highest saliency)
+            
+            # if part of the crop falls outside the image, then move the center of the crop.
+            # It makes sure that the sampled center is within the image (not necesarily the center)
+            if idx_cy + h_crop // 2 > height:
+                diff_cy = idx_cy + h_crop // 2 - height
+                idx_cy -= diff_cy
+            if idx_cy - h_crop // 2 < 0:
+                diff_cy = h_crop // 2 - idx_cy
+                idx_cy += diff_cy
+            if idx_cx + w_crop // 2 > width:
+                diff_cx = idx_cx + w_crop // 2 - width
+                idx_cx -= diff_cx
+            if idx_cx - w_crop // 2 < 0:
+                diff_cx = w_crop // 2 - idx_cx
+                idx_cx += diff_cx
+            
+            idx_y = idx_cy - h_crop // 2
+            idx_x = idx_cx - w_crop // 2
+
+
+            # make sure the complete crop is within the image (take into accounr the top left corner and the size of the crop)
+            if 0 <= idx_x and idx_x + w_crop <= width and 0 <= idx_y and idx_y + h_crop <= height:
+                break
+            elif i == 9: # if it fails 10 times, then just take the center of the image
+                idx_cx = width // 2
+                idx_cy = height // 2
+                idx_x = idx_cx - w_crop // 2
+                idx_y = idx_cy - h_crop // 2
+        
+        all_crops[b] = torch.tensor([idx_x, idx_y, w_crop, h_crop])
+    
+    return all_crops 
+
+
+
 # seed everything
-seed =0 
+seed = 0
 os.environ["PYTHONHASHSEED"] = str(seed)
 np.random.seed(seed)
 torch.manual_seed(seed)
@@ -105,7 +204,7 @@ cudnn.benchmark = False
 pretrained_folder = "output/resnet18_barlowtwins_zca6filters_kernel3_eps0.01/"
 zca_outchannels = 6
 zca_kernel_size = 3
-pool_kernel_size = 32
+pool_kernel_size = 8
 stride = 1
 weighted = True
 save_dir = os.path.join(pretrained_folder, "saliency_maps")
@@ -122,17 +221,16 @@ data_path = args["data_path"]
 print('data_path:', data_path)
 mean=[0.485, 0.456, 0.406]
 std=[1.0, 1.0, 1.0]
-transform = transforms.Compose([
+zca_transform = transforms.Compose([
             transforms.Resize((224,224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=mean, std=std)
             ])
-train_dataset = datasets.ImageFolder(root=os.path.join(data_path, "train"), transform=transform)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=512, shuffle=True)
+zca_dataset = datasets.ImageFolder(root=os.path.join(data_path, "train"), transform=zca_transform)
+zca_loader = torch.utils.data.DataLoader(zca_dataset, batch_size=512, shuffle=True)
 
 ### Get batch of images
-batch_image, batch_label = next(iter(train_loader))
-
+batch_zca_image, _ = next(iter(zca_loader))
 
 ### Load model
 model = eval(args["model_name"])(num_classes=args["num_classes"], conv0_flag=True, conv0_outchannels=zca_outchannels, conv0_kernel_size=zca_kernel_size)
@@ -140,22 +238,40 @@ pretranined_model = args["model_name"] + '_best.pth'
 model_state_dict = torch.load(os.path.join(pretrained_folder,pretranined_model))
 model.load_state_dict(model_state_dict)
 model.to('cuda')
-
-
-### Get feature maps after conv0 layer (absolute values) (also the mean and L2 pool versions)
 model.eval()
+
+### Get GGD params for a batch_zca_image (no pooling, meanpool and L2 pool)
+with torch.no_grad():
+    batch_zca_feats = model.conv0(batch_zca_image.to('cuda')).squeeze()
+batch_abs_zca_feats = batch_zca_feats.abs().detach().cpu()
+batch_meanpool_zca_feats = mean_pooling_batch(batch_abs_zca_feats, kernel_size=pool_kernel_size, stride=stride)
+batch_l2pool_zca_feats = l2_pooling_batch(batch_abs_zca_feats, kernel_size=pool_kernel_size, stride=stride)
+
+ggd_params = fit_gennorm_to_batch(batch_abs_zca_feats)
+meanpool_ggd_params = fit_gennorm_to_batch(batch_meanpool_zca_feats)
+l2pool_ggd_params = fit_gennorm_to_batch(batch_l2pool_zca_feats)
+
+
+
+
+
+# ### Load a training batch
+# trainig_transform = transforms.Compose([
+#             transforms.Resize((224,224)),
+#             transforms.ToTensor(),
+#             transforms.Normalize(mean=mean, std=std)
+#             ])
+# training_dataset = datasets.ImageFolder(root=os.path.join(data_path, "train"), transform=trainig_transform)
+# training_loader = torch.utils.data.DataLoader(training_dataset, batch_size=64, shuffle=True)
+# batch_image, _ = next(iter(training_loader))
+batch_image = batch_zca_image
+
+### Get features for the batch
 with torch.no_grad():
     batch_feats = model.conv0(batch_image.to('cuda')).squeeze()
 batch_abs_feats = batch_feats.abs().detach().cpu()
 batch_meanpool_feats = mean_pooling_batch(batch_abs_feats, kernel_size=pool_kernel_size, stride=stride)
 batch_l2pool_feats = l2_pooling_batch(batch_abs_feats, kernel_size=pool_kernel_size, stride=stride)
-
-
-### Get ggd params for the batch
-ggd_params = fit_gennorm_to_batch(batch_abs_feats)
-meanpool_ggd_params = fit_gennorm_to_batch(batch_meanpool_feats)
-l2pool_ggd_params = fit_gennorm_to_batch(batch_l2pool_feats)
-
 
 ### Compute saliency maps for the batch
 batch_saliency_maps = compute_saliency_map_ggd_batch(batch_abs_feats, ggd_params, weighted=weighted)
@@ -167,40 +283,55 @@ original_shape = (224, 224)
 batch_meanpool_saliency_maps = resize_saliency_map_batch(batch_meanpool_saliency_maps, original_shape)
 batch_l2pool_saliency_maps = resize_saliency_map_batch(batch_l2pool_saliency_maps, original_shape)
 
+### Get crops for the batch
+batch_crops = smart_crop_batch(batch_saliency_maps)#, scale=[0.08, 0.09])
+batch_meanpool_crops = smart_crop_batch(batch_meanpool_saliency_maps)#, scale=[0.08, 0.09])
+batch_l2pool_crops = smart_crop_batch(batch_l2pool_saliency_maps)#, scale=[0.08, 0.09])
+
+# TODO: plot resized crops
+
+
 
 ### Plot image and saliency map for one image in the batch
 for idx in range(50): # [1,8,12,13,14,16,20]
 # idx = 5 # 5 (bad) 8 (good) 23 (bad) 24 (nice)
     image = batch_image[0+idx:1+idx]
 
-    # Get unorm image, saliency map for that image, plot and save
     unnorm_image = image * (torch.tensor(std).view(-1, 1, 1)) + torch.tensor(mean).view(-1, 1, 1)
     unnorm_image = unnorm_image.squeeze().numpy()
     unnorm_image = np.moveaxis(unnorm_image, 0, -1)
+
+    zca_image = batch_feats[idx,:3,:,:].cpu().detach().numpy()
+    zca_image = (zca_image - zca_image.min()) / (zca_image.max() - zca_image.min())
+    zca_image = np.moveaxis(zca_image, 0, -1)
 
     saliencymap_image = batch_saliency_maps[idx].numpy()
     saliencymap_meanpool_image = batch_meanpool_saliency_maps[idx].numpy()
     saliencymap_l2pool_image = batch_l2pool_saliency_maps[idx].numpy()
 
-
-
     # plot
-    plt.figure(figsize=(40, 8))
+    plt.figure(figsize=(24, 24))
 
     # plot original image
-    plt.subplot(1,5,1)
+    plt.subplot(3,3,1)
     plt.imshow(unnorm_image)
     plt.title('Original', fontsize=20)
     plt.axis('off')
 
+    # plot zca version
+    plt.subplot(3,3,2)
+    plt.imshow(zca_image)
+    plt.title('ZCA (3 first channels)', fontsize=20)
+    plt.axis('off')
+
     # plot mean of batch_abs of idx image
-    plt.subplot(1,5,2)
+    plt.subplot(3,3,3)
     plt.imshow(batch_abs_feats[idx].mean(0).cpu().detach().numpy())
     plt.title(f'Mean Abs feat', fontsize=20)
     plt.axis('off')
 
     # plot saliency map
-    plt.subplot(1,5,3)
+    plt.subplot(3,3,4)
     plt.imshow(unnorm_image.mean(2), cmap='gray')
     plt.imshow(saliencymap_image, cmap='jet', alpha=0.5, interpolation='nearest')
     if weighted:
@@ -210,7 +341,7 @@ for idx in range(50): # [1,8,12,13,14,16,20]
     plt.axis('off')
 
     # plot saliency map (mean pool)
-    plt.subplot(1,5,4)
+    plt.subplot(3,3,5)
     plt.imshow(unnorm_image.mean(2), cmap='gray')
     plt.imshow(saliencymap_meanpool_image, cmap='jet', alpha=0.5, interpolation='nearest')
     if weighted:
@@ -220,13 +351,37 @@ for idx in range(50): # [1,8,12,13,14,16,20]
     plt.axis('off')
 
     # plot saliency map (L2 pool)
-    plt.subplot(1,5,5)
+    plt.subplot(3,3,6)
     plt.imshow(unnorm_image.mean(2), cmap='gray')
     plt.imshow(saliencymap_l2pool_image, cmap='jet', alpha=0.5, interpolation='nearest')
     if weighted:
         plt.title(f'Weighted GGD (L2 pool, kernel: {pool_kernel_size})', fontsize=20)
     else:
         plt.title(f'GGD (L2 pool)', fontsize=20)
+    plt.axis('off')
+
+    # plot image, saliency map, and a bounding box for the crop
+    plt.subplot(3,3,7)
+    plt.imshow(unnorm_image)
+    plt.imshow(saliencymap_image, cmap='jet', alpha=0.5, interpolation='nearest')
+    plt.gca().add_patch(plt.Rectangle((batch_crops[idx][0], batch_crops[idx][1]), batch_crops[idx][2], batch_crops[idx][3], linewidth=2, edgecolor='r', facecolor='none'))
+    plt.title('Crop', fontsize=20)
+    plt.axis('off')
+
+    # plot image, saliency map, and a bounding box for the crop (mean pool)
+    plt.subplot(3,3,8)
+    plt.imshow(unnorm_image)
+    plt.imshow(saliencymap_meanpool_image, cmap='jet', alpha=0.5, interpolation='nearest')
+    plt.gca().add_patch(plt.Rectangle((batch_meanpool_crops[idx][0], batch_meanpool_crops[idx][1]), batch_meanpool_crops[idx][2], batch_meanpool_crops[idx][3], linewidth=2, edgecolor='r', facecolor='none'))
+    plt.title('Crop (mean pool)', fontsize=20)
+    plt.axis('off')
+
+    # plot image, saliency map, and a bounding box for the crop (L2 pool)
+    plt.subplot(3,3,9)
+    plt.imshow(unnorm_image)
+    plt.imshow(saliencymap_l2pool_image, cmap='jet', alpha=0.5, interpolation='nearest')
+    plt.gca().add_patch(plt.Rectangle((batch_l2pool_crops[idx][0], batch_l2pool_crops[idx][1]), batch_l2pool_crops[idx][2], batch_l2pool_crops[idx][3], linewidth=2, edgecolor='r', facecolor='none'))
+    plt.title('Crop (L2 pool)', fontsize=20)
     plt.axis('off')
 
     # save plot
