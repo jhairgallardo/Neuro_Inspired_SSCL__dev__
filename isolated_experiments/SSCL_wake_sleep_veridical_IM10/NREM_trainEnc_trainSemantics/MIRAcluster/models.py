337d822a99a4e6610085cbd471c9acd9533deb4b
jhairgallardo <jhairgallardo@gmail.com>
2025-02-25 15:18:54 -0500

    add bias option to conv1x1

--- isolated_experiments/SSCL_wake_sleep_veridical_IM10/NREM_trainEnc_trainSemantics/MIRAcluster/models.py
+++ isolated_experiments/SSCL_wake_sleep_veridical_IM10/NREM_trainEnc_trainSemantics/MIRAcluster/models.py
@@ -72,9 +72,9 @@ def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, d
     )
 
 
-def conv1x1(in_planes: int, out_planes: int, stride: int = 1):
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1, bias: bool = False):
     """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=bias)
 
 
 class BasicBlock(nn.Module):
