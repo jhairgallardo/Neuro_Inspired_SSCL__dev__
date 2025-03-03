8c9582d0d85dab7d22ac29679bb63b5969e5e93a
jhairgallardo <jhairgallardo@gmail.com>
2025-03-03 12:20:38 -0500

    CRCL experiments IM10 wake-sleep veridical

---
+++ isolated_experiments/SSCL_wake_sleep_veridical_IM10/NREM_trainEnc_trainSemantics/CRCLcluster/loss_functions.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import math
+
+class CRCL_PCLoss(nn.Module):
+    def __init__(self, batch_size, alpha, device):
+        super(CRCL_PCLoss, self).__init__()
+        self.batch_size = batch_size
+        self.device = device
+        self.alpha = alpha
+
+        self.mask = self.mask_correlated_samples(batch_size)
+        self.criterion = nn.CrossEntropyLoss(reduction="sum")
+
+    def mask_correlated_samples(self, batch_size):
+        N = 2 * batch_size
+        mask = torch.ones((N, N))
+        mask = mask.fill_diagonal_(0)
+        for i in range(batch_size):
+            mask[i, batch_size + i] = 0
+            mask[batch_size + i, i] = 0
+        mask = mask.bool()
+        return mask
+
+    def forward(self, c_i, c_j):
+        # loss to avoid cluster collapse
+        p_i = c_i.sum(0).view(-1)
+        p_i /= p_i.sum()
+        ne_i = math.log(p_i.size(0)) + (p_i * torch.log(p_i)).sum()
+        p_j = c_j.sum(0).view(-1)
+        p_j /= p_j.sum()
+        ne_j = math.log(p_j.size(0)) + (p_j * torch.log(p_j)).sum()
+        ne_loss = ne_i + ne_j
+
+        # smooth probabilities
+        C = c_i.shape[1]
+        c_i = (1.0 - 0.01) * c_i + 0.01 * (1.0 / C)
+        c_j = (1.0 - 0.01) * c_j + 0.01 * (1.0 / C)
+
+        # loss to pull positive samples together and negative samples apart
+        N = 2 * self.batch_size
+        c = torch.cat((c_i, c_j), dim=0)
+
+        sim = torch.matmul(c, c.T).log()
+        sim_i_j = torch.diag(sim, self.batch_size)
+        sim_j_i = torch.diag(sim, -self.batch_size)
+
+        positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(N, 1)
+        negative_samples = sim[self.mask].reshape(N, -1)
+
+        labels = torch.zeros(N).to(positive_samples.device).long()
+        logits = torch.cat((positive_samples, negative_samples), dim=1)
+        loss = self.criterion(logits, labels)
+        loss /= N
+
+        return loss + self.alpha*ne_loss
+    
+    
+
+
+    
---
+++ isolated_experiments/SSCL_wake_sleep_veridical_IM10/NREM_trainEnc_trainSemantics/CRCLcluster/wake_sleep_trainer.py
@@ -0,0 +1,236 @@
+import os
+
+import torch
+import torch.nn.functional as F
+import torchvision
+from torch.utils.data import WeightedRandomSampler
+from torch.cuda.amp import autocast
+
+from evaluate_cluster import evaluate as eval_pred
+from utils import statistics
+
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import pandas as pd
+from tqdm import tqdm
+import colorcet as cc
+
+class Wake_Sleep_trainer:
+    def __init__(self, 
+                 view_encoder, 
+                 semantic_memory, 
+                 episode_batch_size,
+                 num_episodes_per_sleep,
+                 num_views,
+                 dataset_mean,
+                 dataset_std,
+                 device,
+                 save_dir,
+                 ):
+        self.view_encoder = view_encoder
+        self.semantic_memory = semantic_memory
+        self.episode_batch_size = episode_batch_size
+        self.num_episodes_per_sleep = num_episodes_per_sleep
+        self.num_views = num_views
+
+        self.dataset_mean = dataset_mean
+        self.dataset_std = dataset_std
+
+        self.device = device
+        self.save_dir = save_dir
+
+        self.episodic_memory_imgs = torch.empty(0)
+        self.episodic_memory_labels = torch.empty(0)
+
+        self.sleep_episode_counter = 0
+    
+    def wake_phase(self, incoming_dataloader):
+        ''' 
+        Collect data in episodic memory
+        '''
+
+        aux_memory_imgs = {}
+        aux_memory_labels = {}
+        for i, (batch_episodes, batch_labels , _ ) in enumerate(tqdm(incoming_dataloader)):
+            batch_episodes_imgs = batch_episodes
+            batch_episodes_labels = batch_labels.unsqueeze(1).repeat(1, batch_episodes_imgs.size(1))
+            # collect episodes
+            aux_memory_imgs[i] = batch_episodes_imgs
+            aux_memory_labels[i] = batch_episodes_labels
+
+        # Concatenate all
+        aux_memory_imgs = torch.cat(list(aux_memory_imgs.values()), dim=0)
+        aux_memory_labels = torch.cat(list(aux_memory_labels.values()), dim=0)
+        self.episodic_memory_imgs = torch.cat([self.episodic_memory_imgs, aux_memory_imgs], dim=0)
+        self.episodic_memory_labels = torch.cat([self.episodic_memory_labels, aux_memory_labels], dim=0).type(torch.LongTensor)
+
+        # Reset sleep iteration counter
+        self.sleep_episode_counter = 0
+
+        return None
+    
+    def sleep(self,
+            optimizers,
+            schedulers,
+            criterions,
+            task_id=None,
+            scaler=None,
+            writer=None):
+        '''
+        Train conditional generator and semantic memory
+        '''
+
+        self.view_encoder.train()
+        self.semantic_memory.train()
+
+        # optimizers, schedulers, and criterions
+        optimizer_enc = optimizers[0]
+        scheduler_enc = schedulers[0]
+        optimizer_sem = optimizers[1]
+        scheduler_sem = schedulers[1]
+        criterion_sem = criterions[0]
+
+        # Sampling weights for weighted random sampler (It defines the probability of each sample to be selected)
+        sampling_weights = torch.ones(len(self.episodic_memory_imgs)) # Uniform sampling
+
+        # Train model a bacth at a time
+        while self.sleep_episode_counter < self.num_episodes_per_sleep:
+            #### --- Sample episodes and actions --- ####
+            batch_episodes_idxs = list(WeightedRandomSampler(sampling_weights, self.episode_batch_size, replacement=False))
+            batch_episodes_imgs = self.episodic_memory_imgs[batch_episodes_idxs].to(self.device)
+
+            #### --- Forward pass --- ####
+            batch_episodes_probs = torch.empty(0).to(self.device)        
+            for v in range(self.num_views):
+                batch_imgs = batch_episodes_imgs[:,v,:,:,:]
+                ### Forward pass Semantic Memory
+                with autocast():
+                    batch_tensors = self.view_encoder(batch_imgs)
+                    batch_probs = self.semantic_memory(batch_tensors)
+                batch_episodes_probs = torch.cat([batch_episodes_probs, batch_probs.unsqueeze(1)], dim=1)
+
+            #### --- Calculate Semantic Memory Loss --- ####
+            loss_sem = 0
+            for v in range(self.num_views-1):
+                loss_sem += criterion_sem(batch_episodes_probs[:,0], batch_episodes_probs[:,v+1])
+            loss_sem /= self.num_views
+            ### -> Total Loss ###
+            loss = loss_sem
+
+            #### --- Backward Pass --- ####
+            optimizer_enc.zero_grad()
+            optimizer_sem.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer_enc)
+            scaler.step(optimizer_sem)
+            scaler.update()
+            scheduler_enc.step()
+            scheduler_sem.step()
+
+            #### --- Update sleep counter --- #####
+            self.sleep_episode_counter += self.episode_batch_size
+
+            #### --- Print metrics --- ####
+            enc_lr = scheduler_enc.get_last_lr()[0]
+            sem_lr = scheduler_sem.get_last_lr()[0]
+            loss_sem_value = loss_sem.item()
+            loss_value = loss.item()
+            first_view_probs = batch_episodes_probs[:,0].detach().cpu()
+            if self.sleep_episode_counter == self.episode_batch_size or self.sleep_episode_counter % (5*self.episode_batch_size) == 0 or self.sleep_episode_counter == self.num_episodes_per_sleep:
+                _, _, mi = statistics(first_view_probs)
+                print(f'Episode [{self.sleep_episode_counter}/{self.num_episodes_per_sleep}]' +
+                      f' -- enc lr: {enc_lr:.6f}' +
+                      f' -- sem lr: {sem_lr:.6f}' +
+                      f' -- mi: {mi.item():.6f}' +
+                      f' -- Loss Semantic Memory: {loss_sem_value:.6f}' +
+                      f' -- Loss Total: {loss_value:.6f}'
+                      )
+                writer.add_scalar('MI', mi.item(), task_id*self.num_episodes_per_sleep + self.sleep_episode_counter)
+                
+            #### --- Plot metrics --- ####
+            writer.add_scalar('enc lr', enc_lr, task_id*self.num_episodes_per_sleep + self.sleep_episode_counter)
+            writer.add_scalar('sem lr', sem_lr, task_id*self.num_episodes_per_sleep + self.sleep_episode_counter)
+            writer.add_scalar('MI', mi.item(), task_id*self.num_episodes_per_sleep + self.sleep_episode_counter)
+            writer.add_scalar('Loss Semantic Memory', loss_sem_value, task_id*self.num_episodes_per_sleep + self.sleep_episode_counter)
+            writer.add_scalar('Total Loss', loss_value, task_id*self.num_episodes_per_sleep + self.sleep_episode_counter)
+
+        # Clean up gradients
+        optimizer_sem.zero_grad()
+
+        return None
+
+def evaluate_semantic_memory(loader,
+                             view_encoder,
+                             semantic_memory,
+                             num_pseudoclasses,
+                             task_id,
+                             device,
+                             plot_clusters,
+                             num_gt_classes,
+                             save_dir):
+        '''
+        Evaluate semantic memory
+        '''
+
+        view_encoder.eval()
+        semantic_memory.eval()
+        
+        all_probs = []
+        all_preds = []
+        all_labels = []
+        with torch.no_grad():
+            for i, (images, targets, _ ) in enumerate(loader):
+                images = images.to(device)
+                probs = semantic_memory(view_encoder(images))
+                preds = torch.argmax(probs, dim=1)
+                all_probs.append(probs.detach().cpu())
+                all_preds.append(preds.detach().cpu())
+                all_labels.append(targets)
+        
+        all_preds = torch.cat(all_preds).numpy()
+        all_indices = np.arange(len(all_preds))
+        all_labels = torch.cat(all_labels).numpy()
+        all_probs = torch.cat(all_probs).numpy()
+
+        calc_cluster_acc = len(np.unique(all_labels)) == num_pseudoclasses
+        nmi, ami, ari, fscore, adjacc, image_match, mapped_preds, top5, purity, hmg, cm, cfi = eval_pred(all_labels.astype(int), all_preds.astype(int), num_pseudoclasses, calc_acc=calc_cluster_acc, total_probs=all_probs)
+
+        if plot_clusters:
+            save_dir_clusters = os.path.join(save_dir, 'val_seen_semantic_clusters')
+            os.makedirs(save_dir_clusters, exist_ok=True)
+
+            dict_class_vs_clusters = {}
+            labels_IDs = np.unique(all_labels)
+            clusters_IDs = np.unique(all_preds)
+            for i in labels_IDs:
+                dict_class_vs_clusters[f'Class {i}'] = []
+                for j in range(num_pseudoclasses):
+                    indices = (all_labels==i) & (all_preds==j)
+                    dict_class_vs_clusters[f'Class {i}'].append(np.sum(indices))
+            palette_labelsID= cc.glasbey_category10[:num_gt_classes]
+
+            label_id_to_color = {label_id: palette_labelsID[idx] for idx, label_id in enumerate(range(num_gt_classes))}
+
+            ### Plot number of samples per cluster with color per class
+            df = pd.DataFrame(dict_class_vs_clusters)
+            color_list = [label_id_to_color[i] for i in labels_IDs]
+            df.plot.bar(stacked=True, color=color_list, figsize=(21,8))
+            plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol= 1 if len(labels_IDs) < 20 else 2)
+            plt.title(f'Number of samples per cluster per class TaskID: {task_id}')
+            plt.xlabel('Cluster ID')
+            plt.ylabel('Number of samples')
+            plt.xticks(rotation=0)
+            plt.savefig(os.path.join(save_dir_clusters, f'taskid_{task_id}_number_samples_per_cluster_per_class.png'), bbox_inches='tight')
+            plt.close()
+            # save df, color_list, labels_IDs
+            df.to_csv(os.path.join(save_dir_clusters, f'taskid_{task_id}_number_samples_per_cluster_per_class.csv'))
+            np.save(os.path.join(save_dir_clusters, f'taskid_{task_id}_color_list.npy'), color_list)
+            np.save(os.path.join(save_dir_clusters, f'taskid_{task_id}_labels_IDs.npy'), labels_IDs)
+
+        #### Gather task metrics ####
+        task_metrics = {'NMI': nmi, 'AMI': ami, 'ARI': ari, 'F': fscore, 'ACC': adjacc, 'ACC-Top5': top5,
+                        'Purity': purity, 'Homogeneity': hmg, 'Completeness': cm, 'Class-Fragmentation': cfi}
+
+        return task_metrics
